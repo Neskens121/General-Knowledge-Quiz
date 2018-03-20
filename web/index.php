73aa6b019c207b1acbbf14f30d86f080f54173fc
Neskens121 <nenadb92@yahoo.com>
2018-03-20 19:21:29 +0100

    testDb14

--- web/index.php
+++ web/index.php
@@ -29,7 +29,39 @@ $app->get('/db', function() use($app) {
 $uri = "mongodb://heroku_7hskhz92:Nb!66336623@ds249545.mlab.com:49545/heroku_7hskhz92";
 $client = new MongoDB\Client($uri);
 var_dump($client);
-  return true;
+/*
+ * First we'll add a few songs. Nothing is required to create the songs
+ * collection; it is created automatically when we insert.
+ */
+$songs = $client->db->songs;
+// To insert a dict, use the insert method.
+$songs->insertMany($seedData);
+/*
+ * Then we need to give Boyz II Men credit for their contribution to
+ * the hit "One Sweet Day".
+*/
+$songs->updateOne(
+    array('artist' => 'Mariah Carey'), 
+    array('$set' => array('artist' => 'Mariah Carey ft. Boyz II Men'))
+);
+/*
+ * Finally we run a query which returns all the hits that spent 10 
+ * or more weeks at number 1. 
+*/
+$query = array('weeksAtOne' => array('$gte' => 10));
+$options = array(
+    "sort" => array('decade' => 1),
+);
+$cursor = $songs->find($query,$options);
+foreach($cursor as $doc) {
+    echo 'In the ' .$doc['decade'];
+    echo ', ' .$doc['song']; 
+    echo ' by ' .$doc['artist'];
+    echo ' topped the charts for ' .$doc['weeksAtOne']; 
+    echo ' straight weeks.', "\n";
+}
+
+return true;
 });
 
 
