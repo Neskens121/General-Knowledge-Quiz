f301a026b56c51f32b234c53a641706de47d0a5e
Neskens121 <nenadb92@yahoo.com>
2018-03-20 19:14:08 +0100

    testDb11

--- web/index.php
+++ web/index.php
@@ -25,10 +25,8 @@ $app->get('/', function() use($app) {
 
 $app->get('/db', function() use($app) {
   echo "s";
-//require '..vendor/autoload.php';
- * Standard single-node URI format: 
- * mongodb://[username:password@]host:port/[database]
- */
+require '..vendor/autoload.php';
+
 $uri = "mongodb://heroku_7hskhz92:Nb!66336623@ds249545.mlab.com:49545/heroku_7hskhz92";
 $client = new MongoDB\Client($uri);
 var_dump($client);
