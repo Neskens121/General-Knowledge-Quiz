9d366343dfb5f1943b0397137bf20cf7b7ff76a6
Nenad Bojkovic <nenad.bojkovic@uapdc.local>
2018-03-21 10:24:35 +0100

    again25

--- web/index.php
+++ web/index.php
@@ -28,7 +28,7 @@ $app->get('/db', function() use($app) {
 $uri = "mongodb://testUser:12345!@ds117539.mlab.com:17539/heroku_wzb3tkp3";
 //$uri = "mongodb://testUser:12345!@ds249545.mlab.com:49545/heroku_7hskhz92";
 $client = new MongoDB\Client($uri);
-//var_dump($client);
+var_dump($client);
 
 $inventory = $client->inventory;
 var_dump($inventory);
