2379d86b1fff169cebdce7d7a7815dc2e6a90b18
Nenad Bojkovic <nenad.bojkovic@uapdc.local>
2018-03-21 08:10:53 +0100

    again05

--- web/index.php
+++ web/index.php
@@ -25,71 +25,18 @@ $app->get('/', function() use($app) {
 
 $app->get('/db', function() use($app) {
   echo "s";
-// Create seed data
-/*$seedData = array(
-    [
-        'decade' => '1970s', 
-        'artist' => 'Debby Boone',
-        'song' => 'You Light Up My Life', 
-        'weeksAtOne' => 10
-    ],
-    [
-        'decade' => '1980s', 
-        'artist' => 'Olivia Newton-John',
-        'song' => 'Physical', 
-        'weeksAtOne' => 10
-    ],
-    [
-        'decade' => '1990s', 
-        'artist' => 'Mariah Carey',
-        'song' => 'One Sweet Day', 
-        'weeksAtOne' => 16
-    ],
-);*/
 
 
 $uri = "mongodb://testUser:12345!@ds249545.mlab.com:49545/heroku_7hskhz92";
 $client = new MongoDB\Client($uri);
 var_dump($client);
-/*
- * First we'll add a few songs. Nothing is required to create the songs
- * collection; it is created automatically when we insert.
- */
+
 $songs = $client->db->songs;
 var_dump($songs);
-echo "xxxxxx";
-// To insert a dict, use the insert method.
-/*$songs->insertMany([
-    [
-        'username' => 'admin',
-        'email' => 'admin@example.com',
-        'name' => 'Admin User',
-    ],
-    [
-        'username' => 'test',
-        'email' => 'test@example.com',
-        'name' => 'Test User',
-    ]
-]);*/
-/*
- * Then we need to give Boyz II Men credit for their contribution to
- * the hit "One Sweet Day".
-*/
-/*$songs->updateOne(
-    array('artist' => 'Mariah Carey'), 
-    array('$set' => array('artist' => 'Mariah Carey ft. Boyz II Men'))
-);*/
-/*
- * Finally we run a query which returns all the hits that spent 10 
- * or more weeks at number 1. 
-*/
 
-/*$query = array('weeksAtOne' => array('$gte' => 10));
-$options = array(
-    "sort" => array('decade' => 1),
-);
-$cursor = $songs->find($query,$options);*/
-$cursor = $songs.find( {} );
+echo "xxxxxx";
+
+$cursor = $songs->find( {} );
 
 
 foreach($cursor as $doc) {
