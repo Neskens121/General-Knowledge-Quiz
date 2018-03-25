d77985b63844ac6c2cd2e1534cf8820509dfa019
Neskens121 <nenadb92@yahoo.com>
2018-03-25 16:14:57 +0200

    testing db driver05

--- web/index.php
+++ web/index.php
@@ -53,7 +53,7 @@ $app->post('/', function() use($app) {
 		$db = $client->heroku_7hskhz92;
 		$cursor = $db->questions->find([],['projection' => ['question' => 1, 'potentialAnswers' => 1]]);
 		$cursor->setTypeMap(['root' => 'array', 'document' => 'array', 'array' => 'array']);
-		var_dump($cursor->toArray());
+		//var_dump($cursor->toArray());
 		$cursor = $cursor->toArray();
 
 		foreach ($cursor as $value) {
