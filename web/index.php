56c3dafd3dbb77087ebbebd7e4653adc822f9a1c
Neskens121 <nenadb92@yahoo.com>
2018-03-25 20:38:56 +0200

    randomArr32

--- web/index.php
+++ web/index.php
@@ -52,7 +52,7 @@ $app->post('/', function() use($app) {
 		$uri = "mongodb://testUser:12345!@ds249545.mlab.com:49545/heroku_7hskhz92";
 		$client = new MongoDB\Client($uri);
 		$db = $client->heroku_7hskhz92;
-		$cursor = $db->questions->find([],['projection' => ['question' => 1, 'potentialAnswers' => 1]]);
+		$cursor = $db->questions->find([],['projection' => []]]);
 		$cursor->setTypeMap(['root' => 'array', 'document' => 'array', 'array' => 'array']);
 		//var_dump($cursor->toArray());
 		$queryResultArr = $cursor->toArray();
