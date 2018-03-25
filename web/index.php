41f849eccdf2233bb4fe8f3409621dee176cf7a9
Neskens121 <nenadb92@yahoo.com>
2018-03-25 20:09:12 +0200

    randomArr26

--- web/index.php
+++ web/index.php
@@ -73,7 +73,7 @@ $app->post('/', function() use($app) {
 			//$testArr[] = $row;
 			foreach ($row as $key=>$value) {
 				if(is_object($value)){
-					$testArr[$key] = ((array)$value['_id']);			
+					$testArr[$key] = ((array)$value);			
 				} else {
 					$testArr[$key] = $value;
 				}
