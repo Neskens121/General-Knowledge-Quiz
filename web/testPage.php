65a663f1f1c5d858b3aae6237b899e8bad2c110e
Neskens121 <nenadb92@yahoo.com>
2018-03-27 20:43:21 +0200

    implementing answer checking 01

--- web/testPage.php
+++ web/testPage.php
@@ -6,17 +6,27 @@ if($_POST){
 	//echo $_POST['myArray'];
 	$currentQuestion = $_POST['currentQuestion'];
 	$answerIndex = $_POST['answerIndex'];
+	$questionNumber = $_POST['questionNumber'];
 	//echo $currentQuestion;
-	require 'quizQuestions.php';
-	foreach ($_SESSION['questionIndexArr'] as $key => $value) {
-		$tempQuestionArr[] = $questions[$value];
+	$uri = "mongodb://testUser:12345!@ds249545.mlab.com:49545/heroku_7hskhz92";
+	$client = new MongoDB\Client($uri);
+	$db = $client->heroku_7hskhz92;
+	$cursor = $db->questions->find([]);
+	$cursor->setTypeMap(['root' => 'array', 'document' => 'array', 'array' => 'array']);
+	$queryResultArr = $cursor->toArray();
+	for($i = 0; $i < count($queryResultArr); $i++){
+		foreach ($queryResultArr[$i] as $key => $value) {
+			if(is_object($value)){
+				$testArr[$i][$key] = ((array)$value)['oid'];
+			} else {
+				$testArr[$i][$key] = $value;
+			}
+		}
 	}
-
-	//echo $currentQuestion + '<br>';
-	//echo $_POST['answerIndex'];
-	//echo $tempQuestionArr[$currentQuestion]['indexOfCorrectAnswer'] == $answerIndex ? 'true' : 'false';
-	$tempQuestionArr[$currentQuestion]['answerCorrectness'] = $tempQuestionArr[$currentQuestion]['indexOfCorrectAnswer'] == $answerIndex;
-	echo json_encode($tempQuestionArr[$currentQuestion]);
+	echo json_encode($testArr);
+	
+	//$tempQuestionArr[$currentQuestion]['answerCorrectness'] = $tempQuestionArr[$currentQuestion]['indexOfCorrectAnswer'] == $answerIndex;
+	//echo json_encode($tempQuestionArr[$currentQuestion]);
 }
 
 
